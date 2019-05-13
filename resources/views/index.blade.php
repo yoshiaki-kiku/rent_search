1017e29b96964e425fb1f9b65b817a488c662437
yoshiaki-kiku <yoshiworks333@gmail.com>
2019-05-13 11:49:50 +0900

    ヘッダー余白の追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
     <a class="navbar-brand" href="#!">賃貸検索</a>
 </nav>
 
-<div class="container my-2 px-0 py-2">
+<div class="container my-1 px-0 py-2">
     <h1>賃貸検索</h1>
 </div>
 
 
-<div class="container my-2 p-4 border shadow">
+<div class="container mt-2 mb-4 p-4 border shadow">
     <h2>地域の選択</h2>
 </div>
 
